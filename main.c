97a63973d35193ca276d2bad986a10f07d240289
D Ribeiro <ribeirodavilemos@gmail.com>
2026-03-03 00:40:51 -0300

    Add main.c to print 'HELLO WORLD'

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("H   H  EEEEE  L      L       OOO        W   W   OOO   RRRR   L      DDDD \n");
+    printf("H   H  E      L      L      O   O       W   W  O   O  R   R  L      D   D\n");
+    printf("HHHHH  EEEE   L      L      O   O       W W W  O   O  RRRR   L      D   D\n");
+    printf("H   H  E      L      L      O   O       WW WW  O   O  R  R   L      D   D\n");
+    printf("H   H  EEEEE  LLLLL  LLLLL   OOO        W   W   OOO   R   R  LLLLL  DDDD \n");
+    return 0;
+}
